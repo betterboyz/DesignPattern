f0a891a77854f95ad80d9ef79698a4b061445cf0
dsz <980770958@qq.com>
2018-08-01 16:10:31 +0800

    组合模式

---
+++ src/main/java/com/dsz/b_structural/d_Composite/Employee.java
@@ -0,0 +1,46 @@
+package com.dsz.b_structural.d_Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建 Employee 类，该类带有 Employee 对象的列表
+ *
+ * @author dsz
+ * @date 18/08/01
+ */
+public class Employee {
+  private String name;
+  private String dept;
+  private int salary;
+  private List<Employee> subordinates;
+
+  //构造函数
+  public Employee(String name, String dept, int salary) {
+    this.name = name;
+    this.dept = dept;
+    this.salary = salary;
+    subordinates = new ArrayList<>();
+  }
+
+  public void add(Employee e) {
+    subordinates.add(e);
+  }
+
+  public void remove(Employee e) {
+    subordinates.remove(e);
+  }
+
+  public List<Employee> getSubordinates() {
+    return subordinates;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+      "name='" + name + '\'' +
+      ", dept='" + dept + '\'' +
+      ", salary=" + salary +
+      '}';
+  }
+}
